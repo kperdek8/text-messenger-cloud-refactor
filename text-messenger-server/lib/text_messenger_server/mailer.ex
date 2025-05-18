598fe4ec6bca1e3531d1c744ee2c2ebf980f784a
Krzysztof Perdek <krzysztofperdek@gmail.com>
2025-05-19 00:16:49 +0200

    Delete old monolith architecture

--- text-messenger-server/lib/text_messenger_server/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule TextMessengerServer.Mailer do
-  use Swoosh.Mailer, otp_app: :text_messenger_server
-end
--- text-messenger-server/lib/text_messenger_server/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule TextMessengerServer.Repo do
-  use Ecto.Repo,
-    otp_app: :text_messenger_server,
-    adapter: Ecto.Adapters.Postgres
-end
